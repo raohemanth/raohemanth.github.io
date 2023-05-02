6364b395b6bde19d33e8140c9456062649e32a3d
Hemanth Rao Karade Nagendra <hemanth@colorado.edu>
2023-05-01 23:30:34 -0600

    update

--- src/components/sections/jobs.js
+++ src/components/sections/jobs.js
@@ -86,7 +86,6 @@ const StyledTabButton = styled.button`
   }
   @media (max-width: 600px) {
     ${({ theme }) => theme.mixins.flexCenter};
-    min-width: 120px;
     padding: 0 15px;
     border-left: 0;
     border-bottom: 2px solid var(--lightest-navy);
