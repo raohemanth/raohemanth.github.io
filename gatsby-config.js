3441341f538af1a7efb1fb799ee2ef47131bcf8a
Hemanth Rao Karade Nagendra <hemanth@colorado.edu>
2023-05-01 21:03:16 -0600

    Removing unnecessary stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: 'raohemanth.github.io',
+  // pathPrefix: '/',
   siteMetadata: {
     title: 'Hemanth Rao',
     description:
