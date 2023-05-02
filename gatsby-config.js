b1d8309c331b63925b84de0a14c1abfb3004a665
Hemanth Rao Karade Nagendra <hemanth@colorado.edu>
2023-05-01 21:24:05 -0600

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'Hemanth Rao',
     description:
       'Hemanth Rao is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://raohemanth.github.io/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@ihemanthrao',
   },
