70cec3089d147b196f63a63559303d704d48494b
Hemanth Rao <h.rao46@gmail.com>
2025-09-16 01:49:50 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-651da8f4b58a71fbfa97.js"
+    "url": "webpack-runtime-8d6846cacde4ed1d0f05.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ed8da0c761688c445752448670a8209c"
+    "revision": "f483aee171f62b5f46ca2f22d3966570"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
